383e9466d1acd1333d3828b4b42a8c47dabbc0a5
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2019-02-27 09:26:16 -0800

    Resolving directory structure dependency for summarize_miso rule. Removing non-pertinent yamls

--- scripts/summarize_miso.py
+++ scripts/summarize_miso.py
@@ -4,7 +4,8 @@ import subprocess
 input = snakemake.input[0]
 path = snakemake.output[0]
 
-out = os.path.join('/'.join(path.split('/')[:-4]))
+input = os.path.join('/'.join(input.split('/')[:-2]))
+out = os.path.join('/'.join(path.split('/')[:-2]))
 
 bash_cmd ="summarize_miso --summarize-samples {input} {out}".format(input=input,out=out)
 
